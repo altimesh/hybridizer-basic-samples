98be4c05b92be4458c7ad5cd370c0d6f0fd7a535
fduguet <florent.duguet@altimesh.com>
2017-03-11 14:29:29 +0100

    Hello World sample

---
+++ HybridizerBasicSamples/1.Simple/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,40 @@
+﻿using Hybridizer.Runtime.CUDAImports;
+using System;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        [EntryPoint("run")]
+        public static void Run(int N, int[] a, int[] b)
+        {
+            Parallel.For(0, N, i => { a[i] += b[i]; });
+        }
+
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3, 4, 5 };
+            int[] b = { 10, 20, 30, 40, 50 };
+
+            // create an instance of HybRunner object to wrap calls on GPU
+            HybRunner runner = HybRunner.Cuda("HelloWorld_CUDA.dll");
+
+            // create a wrapper object to call GPU methods instead of C#
+            dynamic wrapped = runner.Wrap(new Program());
+
+            // run the method on GPU
+            wrapped.Run(a.Length, a, b);
+
+            // verify the results
+            for (int k = 0; k < a.Length; ++k)
+            {
+                if (a[k] != (11 * (k+1)))
+                {
+                    Console.Out.WriteLine("ERROR !");
+                }
+            }
+            Console.Out.WriteLine("DONE");
+        }
+    }
+}
