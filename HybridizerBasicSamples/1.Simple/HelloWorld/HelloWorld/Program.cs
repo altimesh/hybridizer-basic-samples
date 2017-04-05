53b2daee7947ff0f457f14172c8e77f879e70a37
axelanglet <langlet.axel@hotmail.fr>
2017-04-05 15:17:43 +0200

    hyb basic samples : attribute randoms doubles to arrays and test if the two results are equals.

--- HybridizerBasicSamples/1.Simple/HelloWorld/HelloWorld/Program.cs
+++ HybridizerBasicSamples/1.Simple/HelloWorld/HelloWorld/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Hybridizer.Runtime.CUDAImports;
 using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace HelloWorld
@@ -14,25 +15,55 @@ namespace HelloWorld
 
         static void Main(string[] args)
         {
-            double[] a = { 1, 2, 3, 4, 5 };
-            double[] b = { 10, 20, 30, 40, 50 };
+            //Initialize a stopwatch
+            Stopwatch timer = new Stopwatch();
+
+            int N = 1024 * 1024 * 64;
+            double[] acuda = new double[N];
+            double[] adotnet = new double[N];
+
+            double[] b = new double[N];
+
+            Random rand = new Random();
+
+            //Initialize acuda et adotnet and b by some doubles randoms, acuda and adotnet have same numbers. 
+            for(int i = 0; i < N; ++i)
+            {
+                acuda[i] = rand.NextDouble();
+                adotnet[i] = acuda[i];
+                b[i] = rand.NextDouble();
+            }
+
 
             // create an instance of HybRunner object to wrap calls on GPU
-            HybRunner runner = HybRunner.Cuda("HelloWorld_CUDA.dll");
+            HybRunner runner = HybRunner.Cuda("HelloWorld_CUDA.dll").SetDistrib(20,256);
 
             // create a wrapper object to call GPU methods instead of C#
             dynamic wrapped = runner.Wrap(new Program());
 
-            // run the method on GPU
-            wrapped.Run(a.Length, a, b);
+            //Start the stopwatch
+            timer.Start();
 
+            // run the method on GPU
+            wrapped.Run(N, acuda, b);
+
+            //Stop the stopwatch
+            timer.Stop();            
+
+            //Restart the stopwatch
+            timer.Restart();
+
+            // run the method on CPU
+            Run(N, adotnet, b);
+
+            //Stop the stopwatch
+            timer.Stop();
+            
             // verify the results
-            for (int k = 0; k < a.Length; ++k)
+            for (int k = 0; k < N; ++k)
             {
-                if (a[k] != (11 * (k+1)))
-                {
+                if (acuda[k] != adotnet[k])
                     Console.Out.WriteLine("ERROR !");
-                }
             }
             Console.Out.WriteLine("DONE");
         }
