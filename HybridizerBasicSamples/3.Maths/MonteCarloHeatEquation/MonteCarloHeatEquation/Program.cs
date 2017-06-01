abce1a7e83b340c0d9e61c44ae980e12737ed826
JeWaVe <regis.portalez@altimesh.com>
2017-06-01 14:26:53 +0200

    montercarlo : finalize

--- HybridizerBasicSamples/3.Maths/MonteCarloHeatEquation/MonteCarloHeatEquation/Program.cs
+++ HybridizerBasicSamples/3.Maths/MonteCarloHeatEquation/MonteCarloHeatEquation/Program.cs
@@ -7,16 +7,32 @@ namespace MonteCarloHeatEquation
 {
     class Program
     {
-        public static float clamp(float x, float min, float max)
+        static void Main(string[] args)
         {
-            return x > min ? ((x < max) ? x : max) : min;
+            const int N = 256;
+            const int iterCount = 100;
+            
+            var problem = new SquareProblem<SimpleWalker, SimpleBoundaryCondition>(N, iterCount);
+            // example of another instanciation
+            // var problem = new TetrisProblem<SimpleWalker, TetrisBoundaryCondition>(N, iterCount);
+
+            cudaDeviceProp prop;
+            cuda.GetDeviceProperties(out prop, 0);
+
+            HybRunner runner = HybRunner.Cuda("MonteCarloHeatEquation_CUDA.dll").SetDistrib(16 * prop.multiProcessorCount, 128);
+            var solver = new MonteCarloHeatSolver(problem);
+            dynamic wrapped = runner.Wrap(solver);
+            //solver.Solve();   // C# version
+            wrapped.Solve();    // generated version
+
+            problem.RefreshHost();
+            problem.SaveImage("result.bmp", GetColor);
+            Process.Start("result.bmp");
         }
 
-        static Color Interpolate(Color a, Color b, float t)
-        {
-            return Color.FromArgb((int) (t * a.R + (1.0F - t) * b.R), (int) (t * a.G + (1.0F - t) * b.G), (int) (t * a.B + (1.0F - t) * b.B));
-        }
-
+        /// <summary>
+        /// from white (warm) to black (cold) following rainbow colors
+        /// </summary>
         static Color GetColor(float temperature)
         {
             int map = (int)Math.Floor(temperature * 8.0F);
@@ -24,32 +40,14 @@ namespace MonteCarloHeatEquation
                 return Color.Black;
             if (temperature >= 1.0F)
                 return Color.White;
-            float t = 8.0F * temperature - (float) Math.Floor(temperature * 8.0F);
+            float t = 8.0F * temperature - (float)Math.Floor(temperature * 8.0F);
             Color[] colors = new Color[9] { Color.Black, Color.Red, Color.Orange, Color.Yellow, Color.Green, Color.Blue, Color.Indigo, Color.Violet, Color.White };
-            return Interpolate(colors[map], colors[map + 1], 1.0F-t);
+            return Interpolate(colors[map], colors[map + 1], t);
         }
 
-        static void Main(string[] args)
+        static Color Interpolate(Color a, Color b, float t)
         {
-            const int N = 256;
-            const int iterCount = 100;
-            
-            //var problem = new TetrisProblem<SimpleWalker, TetrisBoundaryCondition>(N, iterCount);
-            // example of another instanciation
-            var problem = new SquareProblem<SimpleWalker, SimpleBoundaryCondition>(N, iterCount);
-
-            cudaDeviceProp prop;
-            cuda.GetDeviceProperties(out prop, 0);
-
-            HybRunner runner = HybRunner.Cuda("MonteCarloHeatEquation_CUDA.dll").SetDistrib(16 * prop.multiProcessorCount, 128);
-            var solver = new MonteCarloHeatSolver(problem);
-            //solver.Solve();
-            dynamic wrapped = runner.Wrap(solver);
-            wrapped.Solve();
-
-            problem.RefreshHost();
-            problem.SaveImage("square.bmp", GetColor);
-            Process.Start("square.bmp");
+            return Color.FromArgb((int)((1.0F - t) * a.R + t * b.R), (int)((1.0F - t) * a.G + t * b.G), (int)((1.0F - t) * a.B + t * b.B));
         }
     }
 }
