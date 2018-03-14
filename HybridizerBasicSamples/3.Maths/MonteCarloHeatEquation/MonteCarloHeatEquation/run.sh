6fccf4650a47b592a846f2bb4ac262986033d607
rportalez <1Alice_moutier>
2018-03-14 11:03:52 +0100

    fix cuda version for cuda8

--- HybridizerBasicSamples/3.Maths/MonteCarloHeatEquation/MonteCarloHeatEquation/run.sh
+++ HybridizerBasicSamples/3.Maths/MonteCarloHeatEquation/MonteCarloHeatEquation/run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-export LD_LIBRARY_PATH=/opt/altimesh/hybridizer-essentials/bin/:/usr/local/cuda-9.0/lib64/:/usr/lib/x86_64-linux-gnu/:${LD_LIBARRY_PATH}
+export LD_LIBRARY_PATH=/opt/altimesh/hybridizer-essentials/bin/:/usr/local/cuda-8.0/lib64/:/usr/lib/x86_64-linux-gnu/:${LD_LIBARRY_PATH}
 
 cd target
 mono ./MonteCarloHeatEquation.exe
-            
