8f2b254a722780563ca512905fee2743915e47d8
rportalez <1Alice_moutier>
2018-06-01 16:27:28 +0200

    add vb sample

---
+++ HybridizerBasicSamples_CUDA92/1.Simple/HelloVB/HelloVB/HelloVB.vb
@@ -0,0 +1,31 @@
+﻿Imports Hybridizer.Runtime.CUDAImports
+
+<Assembly: HybRunnerDefaultSatelliteName("HelloVB_CUDA.dll")>
+
+Class HelloVB
+
+    <EntryPoint>
+    Shared Sub Add(ByVal a As Single(), ByVal b As Single(), ByVal N As Integer)
+        For i As Integer = 0 To N - 1
+            a(i) += b(i)
+        Next
+    End Sub
+
+    Shared Sub Main()
+        Dim N As Integer = 32
+        Dim a(N - 1) As Single
+        Dim b(N - 1) As Single
+
+        For i As Integer = 0 To N - 1
+            a(i) = 1.0F
+            b(i) = i
+        Next
+
+        Dim wrapped As Object = HybRunner.Cuda().Wrap(New HelloVB())
+
+        wrapped.Add(a, b, N)
+
+        cuda.DeviceSynchronize()
+        Console.Out.WriteLine(String.Join(", ", a))
+    End Sub
+End Class
