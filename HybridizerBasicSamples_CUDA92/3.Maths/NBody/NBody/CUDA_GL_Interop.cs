316ae4d863a3a69165963eda7d2773353751ba6f
rportalez <1Alice_moutier>
2018-06-11 14:07:33 +0200

    add NBody simulation for opengl interop

---
+++ HybridizerBasicSamples_CUDA92/3.Maths/NBody/NBody/CUDA_GL_Interop.cs
@@ -0,0 +1,33 @@
+﻿using Hybridizer.Runtime.CUDAImports;
+using System;
+using System.Runtime.InteropServices;
+
+namespace NBody
+{
+    public class CUDA_GL_Interop
+    {
+        [DllImport("cudart64_92.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern cudaError_t cudaGLRegisterBufferObject(uint buffer);
+
+        [DllImport("cudart64_92.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern cudaError_t cudaGraphicsGLRegisterBuffer(out IntPtr pCudaResource, uint buffer, uint Flags);
+
+        [DllImport("cudart64_92.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern cudaError_t cudaGraphicsUnregisterResource(IntPtr resource);
+
+        [DllImport("cudart64_92.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern cudaError_t cudaGLUnregisterBufferObject(uint buffer);
+
+        [DllImport("cudart64_92.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern cudaError_t cudaGraphicsResourceGetMappedPointer(out IntPtr devPtr, out size_t size, IntPtr resource);
+
+        [DllImport("cudart64_92.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern cudaError_t cudaGraphicsResourceSetMapFlags(IntPtr resource, uint flags);
+
+        [DllImport("cudart64_92.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern cudaError_t cudaGraphicsMapResources(int count, IntPtr[] resources, cudaStream_t stream);
+
+        [DllImport("cudart64_92.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern cudaError_t cudaGraphicsUnmapResources(int count, IntPtr[] resources, cudaStream_t stream);
+    }
+}
