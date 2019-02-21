42b99c6baa0db0d698d06ff2b70ae7f6305c6e81
Regis Portalez <regis.portalez@altimesh.com>
2019-02-21 11:49:52 +0100

    fix generic functions sample

--- HybridizerBasicSamples_CUDA100/7.Advanced/GenericFunctions/GenericFunctions/Program.cs
+++ HybridizerBasicSamples_CUDA100/7.Advanced/GenericFunctions/GenericFunctions/Program.cs
@@ -5,22 +5,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/// <summary>
+/// experimental feature -- no extensive testing
+/// </summary>
 namespace GenericFunctions
 {
-    class Vector<T> where T: struct
+    class Vector<T> where T : struct
     {
-        T[] _data;
-        int _length;
+        readonly T[] _data;
 
         public Vector(T[] data)
         {
             _data = data;
-            _length = data.Length;
+            Length = data.Length;
         }
 
         [Kernel]
-        public int Length { get => _length; }
-
+        public int Length { get; }
+        
         [Kernel]
         public T this[int i] { get => _data[i]; set { _data[i] = value; } }
     }
@@ -34,13 +36,9 @@ namespace GenericFunctions
         }
 
         [EntryPoint]
-        public static void AddOne(Vector<float> v)
+        public static void Add(Vector<float> v, float a)
         {
-            int N = v.Length;
-            for (int i = threadIdx.x + blockDim.x * blockIdx.x; i < N; i += blockDim.x * gridDim.x)
-            { 
-                Invoke(v, i, 1.0F, (x, y) => x + y);
-            }
+            Parallel.For(0, v.Length, i => Invoke<float>(v, i, a, (x, y) => x + y));
         }
     }
 
@@ -48,21 +46,22 @@ namespace GenericFunctions
     {
         static void Main(string[] args)
         {
-            const int N = 1024 * 1024 * 32;
+            const int N = 1024*1024*32;
             float[] a = new float[N];
-
-            HybRunner.Cuda().Wrap(new Operations()).AddOne(new Vector<float>(a));
-
-            for(int i = 0; i < N; ++i)
+            var vect = new Vector<float>(a);
+            HybRunner.Cuda().Wrap(new Operations()).Add(vect, 1.0F);
+            cuda.DeviceSynchronize();
+            
+            for (int i = 0; i < N; ++i)
             {
-                if(a[i] != 1.0F)
+                if (a[i] != 1.0F)
                 {
                     Console.Error.WriteLine($"ERROR at {i} : got {a[i]} instead of 1.0F");
                     Environment.Exit(6); // abort
                 }
             }
 
-            Console.Out.Write("OK");
+            Console.Out.WriteLine("OK");
         }
     }
 }
