3904ae9fbf0e24fe752a7daf051b4e191df17de3
Portalez Régis <11440561+JeWaVe@users.noreply.github.com>
2022-11-24 13:23:14 +0100

    finish unification of VS solutions and CUDA versions into - one solution - two config builds

---
+++ HybridizerBasicSamples_CUDA/1.Simple/Builtin/Builtin/Program.cs
@@ -0,0 +1,41 @@
+﻿using Hybridizer.Runtime.CUDAImports;
+using System;
+using System.Configuration;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Builtin
+{
+    class Program
+    {
+        [EntryPoint]
+        public static void Run(int N, int[] a, int[] result)
+        {
+            Parallel.For(0, N, i => 
+            {
+                Interlocked.Add(ref result[0], a[i]);
+            });
+        }
+
+        static void Main(string[] args)
+        {
+            const int N = 1024;
+            int[] a = new int[N];
+            for(int i = 0; i < 1024; ++i)
+            {
+                a[i] = i;
+            }
+
+            int[] result = new int[1];
+            
+            HybRunner runner = HybRunner.Cuda();
+
+            // create a wrapper object to call GPU methods instead of C#
+            dynamic wrapped = runner.Wrap(new Program());
+
+            wrapped.Run(N, a, result);
+            cuda.DeviceSynchronize();
+            Console.Out.WriteLine("sum = {0}", result[0]);
+        }
+    }
+}
--- HybridizerBasicSamples_CUDA116/1.Simple/Intrinsics/Intrinsics/README.txt
+++
@@ -1,5 +0,0 @@
-﻿This is sample of mixed precision arithmetic from C#. 
-
-GPU support mixed precision starting from computing capability 5.3, which means Volta, Pascal, and Jetson TX1
-
-Before that (Maxwell, Kepler, Fermi...), the generated code won't compile. Therefore, the project is not built as part of the solution. 
